8e5d7955178f81402334b2f28cb137b6beb66195
marco-soria <marco.soriaa7@gmail.com>
2024-09-03 11:00:16 -0500

    file upload

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import { Authroutes } from './auth/routes';
 import { CategoryRoutes } from './category/routes';
 import { ProductRoutes } from './products/routes';
+import { FileUploadRoutes } from './file-upload/routes';
+import { ImageRoutes } from './images/routes';
 
 
 
@@ -18,7 +20,9 @@ export class AppRoutes {
     router.use('/api/auth', Authroutes.routes );
     router.use('/api/categories', CategoryRoutes.routes );
     router.use('/api/products', ProductRoutes.routes );
-
+    router.use('/api/upload', FileUploadRoutes.routes );
+    router.use('/api/images', ImageRoutes.routes );
+    
 
 
     return router;
