d5d9bdd0e81bee68f21e2690daf3bccf91b8d484
marco-soria <marco.soriaa7@gmail.com>
2024-09-03 10:13:40 -0500

    auth and middleware

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,11 @@
 
 
 
+
+
+
 export * from './mongo/mongo-database';
 
+export * from './mongo/models/category.model';
+export * from './mongo/models/product.model';
 export * from './mongo/models/user.model';
